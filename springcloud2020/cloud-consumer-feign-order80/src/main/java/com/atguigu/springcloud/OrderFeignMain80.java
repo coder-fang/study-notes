a2d4b039070201de9cee0905571e3abaacbe58db
coder-fang <1846015350@qq.com>
2021-12-20 15:15:27 +0800

    openfeign server use

---
+++ springcloud2020/cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+/**
+ * @author LiFang
+ * @version 1.0
+ * @since 2021/12/20 14:58
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class, args);
+    }
+}
