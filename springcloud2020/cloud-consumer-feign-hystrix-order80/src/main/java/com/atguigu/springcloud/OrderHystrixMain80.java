060b4c215f71be54ee5eb8b687333c485ff0529b
coder-fang <1846015350@qq.com>
2021-12-20 17:21:32 +0800

    hystrix test Concurrency

---
+++ springcloud2020/cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author LiFang
+ * @version 1.0
+ * @since 2021/12/20 17:04
+ */
+@SpringBootApplication
+@EnableFeignClients
+//@EnableHystrix
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class, args);
+    }
+}
