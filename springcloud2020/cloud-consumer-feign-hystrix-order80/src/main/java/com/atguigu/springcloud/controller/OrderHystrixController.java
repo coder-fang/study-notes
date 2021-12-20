97cdca6cb4af075a0811b985ad10c3e0c683c7ec
coder-fang <1846015350@qq.com>
2021-12-20 22:36:44 +0800

    hystrix 通配服务降级 fallback

--- springcloud2020/cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ springcloud2020/cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,17 +1,20 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.annotation.Resource;
 
 @RestController
 @Slf4j
-public class OrderHystirxController {
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
+public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
 
@@ -21,10 +24,11 @@ public class OrderHystirxController {
         return result;
     }
 
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
-    })
+    //    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+//    })      // 独立服务降级方法
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand   //用全局的fallback方法
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         //int age = 10/0;
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
@@ -34,5 +38,10 @@ public class OrderHystirxController {
     //善后方法
     public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
         return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+    }
+
+    //全局fallback方法
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~";
     }
 }
