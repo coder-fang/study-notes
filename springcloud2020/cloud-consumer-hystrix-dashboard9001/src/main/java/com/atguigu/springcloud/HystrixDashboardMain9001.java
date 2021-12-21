f7b302e6a3df371217ef04cb636e43dc2755c1bb
coder-fang <1846015350@qq.com>
2021-12-21 14:46:59 +0800

    64 hystrix dashboard

---
+++ springcloud2020/cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author LiFang
+ * @version 1.0
+ * @since 2021/12/21 9:58
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
