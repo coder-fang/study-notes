29033e0630402546941af2d3b8c6a661a1b4dcd2
coder-fang <1846015350@qq.com>
2021-12-31 22:31:47 +0800

    config center

--- springcloud2020/cloud-provider-hygtrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ springcloud2020/cloud-provider-hygtrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,7 +1,5 @@
 package com.atguigu.springcloud.controller;
 
-import com.atguigu.springcloud.entities.CommonResult;
-import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -41,16 +39,5 @@ public class PaymentController {
         String result = paymentService.paymentCircuitBreaker(id);
         log.info("****result:" + result);
         return result;
-    }
-
-    @GetMapping(value = "/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id) {
-        Payment payment = paymentService.getPaymentById(id);
-        log.info("*****插入结果：" + payment);
-        if (payment != null) {
-            return new CommonResult(200, "查询成功,serverPort:" + serverPort, payment);
-        } else {
-            return new CommonResult(444, "没有对应记录,查询ID：" + id, null);
-        }
     }
 }
