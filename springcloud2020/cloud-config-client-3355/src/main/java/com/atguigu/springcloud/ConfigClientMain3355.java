01047c282931fa6446f17fa9eebb2d66e98fb2ed
coder-fang <1846015350@qq.com>
2022-01-02 15:48:07 +0800

    config center

---
+++ springcloud2020/cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author LiFang
+ * @version 1.0
+ * @since 2022/1/1 18:02
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
+
